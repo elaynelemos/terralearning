a8c5ddc06f5871df88b911cccbcbcbac163aa6bf
elaynelemos <elayne.l.lemos@gmail.com>
2021-06-21 10:40:43 -0300

    Terraform hello world

--- main.tf
+++ main.tf
@@ -1,15 +1,40 @@
 terraform {
-  backend "http" {}
-
-  required_providers{
-    "aws" {
+  required_providers {
+    aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.45"
     }
   }
 }
 
 provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
+  alias  = "sa_east_1"
+  region = "sa-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-05aa753c043f1dcd3"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+    Name = "terraform-hello-world"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-hello-world"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
