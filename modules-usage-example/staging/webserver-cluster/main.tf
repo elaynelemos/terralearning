40a126a87f220f7cfa7a5e2105c810f99d9d7a29
elaynelemos <elayne.l.lemos@gmail.com>
2021-07-01 08:06:52 -0300

    fix: backend config

--- modules-usage-example/staging/webserver-cluster/main.tf
+++ modules-usage-example/staging/webserver-cluster/main.tf
@@ -1,9 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket = "elemos-terralearning-state"
+    key    = "modules-usage-example/staging/services/webserver-cluser/terraform.tfstate"
+    region = "sa-east-1"
+
+    dynamodb_table = "elemos-terralearning-state-locks"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "sa-east-1"
 }
 
 module "webserver_cluster" {
-  source = "github.com/elaynelemos/terralearning-modules//webserver-cluster?ref=v0.0.1"
+  source = "github.com/elaynelemos/terralearning-modules//services//webserver-cluster?ref=v0.0.2"
 
   cluster_name = "webservers-module-ex-staging"
   instance_type = "t2.micro"
